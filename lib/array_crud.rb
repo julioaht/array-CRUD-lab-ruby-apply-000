000e2fc2b091536ff0a8ac02ce8af58001e12759
julio hernandez <julioandres72@gmail.com>
2020-08-23 17:52:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-create_an_array<<"arrays!"
+[1,2,3,4]<<"arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
@@ -19,7 +19,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-1,"wow"
+[1,"wow", "arrays"].shift
 end
 
 def retrieve_element_from_index(array, index_number)
